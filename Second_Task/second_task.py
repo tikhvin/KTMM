2aa670e7c2c96502e6c1920e18c3be6f5c573933
Daniil Tikhvinsky <torrentinod@gmail.com>
2024-04-21 21:55:19 +0300

    Second task is still under construction

--- Second_Task/second_task.py
+++ Second_Task/second_task.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.integrate import odeint
 from sympy import Symbol, solve, lambdify, Matrix
 
 kp1 = Symbol("kp1")
@@ -18,11 +19,9 @@ eq2 = kp3 * (1 - x - y) ** 2 - km3 * (y ** 2)
 res = solve([eq1, eq2], x, kp1)
 x_eq = res[0][0]
 kp1_eq = res[0][1]
-# kp1_diff_eq = diff(res[0][1], y)
 
-x_func = lambdify((y, km3, kp3), x_eq)
-kp1_func = lambdify((y, km1, kp2, kp3, km3), kp1_eq)
-# kp1_diff_func = lambdify((y, km1, kp2, kp3, km3), kp1_diff_eq)
+x_lambdify = lambdify((y, km3, kp3), x_eq)
+kp1_lambdify = lambdify((y, km1, kp2, kp3, km3), kp1_eq)
 
 kp1_val = 0.12
 km1_val = 0.005
@@ -30,86 +29,161 @@ kp2_val = 1.05
 kp3_val = 0.0032
 km3_val = 0.002
 
-x_f = x_func(np.linspace(0, 1, 1001), km3_val, kp3_val)
-func = kp1_func(np.linspace(0, 1, 1001), km1_val, kp2_val, kp3_val, km3_val)
-# func_diff = kp1_diff_func(np.linspace(0, 1, 1001), km1_val, kp2_val, kp3_val, km3_val)
+x_function = x_lambdify(np.linspace(0.01, 1, 1000), km3_val, kp3_val)
+kp1_function = kp1_lambdify(np.linspace(0.01, 1, 1000), km1_val, kp2_val, kp3_val, km3_val)
 
-plt.plot(np.linspace(0, 1, 1001), x_f)
+plt.plot(np.linspace(0.01, 1, 1000), x_function, label='x')
+plt.plot(np.linspace(0.01, 1, 1000), kp1_function, label='k_1')
+plt.title("Зависимости функций x и k_1 от у")
 plt.xlabel("y")
-plt.ylabel("x")
-plt.show()
-
-plt.plot(np.linspace(0, 1, 1001), func)
-plt.xlabel("y")
-plt.ylabel("k_1")
-plt.show()
-
-A = Matrix([eq1, eq2])
-var_vector = Matrix([x, y])
-jacA = A.jacobian(var_vector)
-det_jacA = jacA.det()
-trace_jacA = jacA.trace()
-
-# trace_func = lambdify((x, y, kp1, km1, kp2, kp3, km3) , trace_jacA)
-
-trace_jacA_wo_x = trace_jacA.subs(x, x_eq) # линия нейтральности
-det_jacA_wo_x = det_jacA.subs(x, x_eq) # линия кратности
-
-det_jacA_wo_x_func = lambdify((y, kp1, km1, kp2, kp3, km3), det_jacA_wo_x)
-det_jacA_wo_x_f = det_jacA_wo_x_func(np.linspace(0, 1, 1001), kp1_val, km1_val, kp2_val, kp3_val, km3_val)
-
-point = 0
-for i in range(len(det_jacA_wo_x_f) - 1):
-    if det_jacA_wo_x_f[i] < 0 and det_jacA_wo_x_f[i+1] > 0:
-        point = i
-
-# plt.plot(np.linspace(0, 1, 1001), det_jacA_wo_x_f)
-# plt.xlabel("y")
-# plt.ylabel("det A")
-# plt.show()
-
-plt.plot(func, x_f, label='x')
-plt.plot(func, np.linspace(0, 1, 1001), label='y')
-plt.plot(func[point], x_f[point], '*')
-plt.plot(func[point], np.linspace(0, 1, 1001)[point], '*')
-plt.xlabel("k_1")
 plt.legend()
 plt.show()
 
-res = solve([trace_jacA_wo_x], kp1)
+matrix = Matrix([eq1, eq2])
+jacobian = matrix.jacobian(Matrix([x, y]))
+
+det_jacobian = jacobian.det()
+trace_jacobian = jacobian.trace()
+
+trace_jacobian_wo_x = trace_jacobian.subs(x, x_eq) # линия нейтральности
+det_jacobian_wo_x = det_jacobian.subs(x, x_eq) # линия кратности
+
+det_jacobian_wo_x_lambdify = lambdify((y, kp1, km1, kp2, kp3, km3), det_jacobian_wo_x)
+det_jacobian_wo_x_function = det_jacobian_wo_x_lambdify(np.linspace(0.01, 1, 1000), kp1_val, km1_val, kp2_val, kp3_val, km3_val)
+
+trace_jacobian_wo_x_lambdify = lambdify((y, kp1, km1, kp2, kp3, km3), trace_jacobian_wo_x)
+trace_jacobian_wo_x_function = trace_jacobian_wo_x_lambdify(np.linspace(0.01, 1, 1000), kp1_val, km1_val, kp2_val, kp3_val, km3_val)
+
+point1 = 0
+point2 = 0
+
+for i in range(len(det_jacobian_wo_x_function) - 1):
+    if det_jacobian_wo_x_function[i] < 0 and det_jacobian_wo_x_function[i+1] > 0:
+        point1 = i
+
+    if det_jacobian_wo_x_function[i] > 0 and det_jacobian_wo_x_function[i+1] < 0:
+        point2 = i
+
+plt.plot(kp1_function, x_function, label='x')
+plt.plot(kp1_function, np.linspace(0.01, 1, 1000), label='y')
+
+plt.plot(kp1_function[point1], x_function[point1], '*')
+plt.plot(kp1_function[point1], np.linspace(0.01, 1, 1000)[point1], '*')
+
+plt.plot(kp1_function[point2], x_function[point2], '*')
+plt.plot(kp1_function[point2], np.linspace(0.01, 1, 1000)[point2], '*')
+
+x_first = x_function[point1]
+y_first = np.linspace(0.01, 1, 1000)[point1]
+
+x_second = x_function[point2]
+y_second = np.linspace(0.01, 1, 1000)[point2]
+
+plt.xlabel("k_1")
+plt.title("Зависимость стационарных состояний от параметра k_1")
+plt.xlim([0, 0.3])
+plt.ylim([-0.1, 1])
+plt.legend()
+plt.show()
+
+def make_system_1(initial_vals, time):
+    x = initial_vals[0]
+    y = initial_vals[1]
+    dx_dt = kp1_function[point1] * (1 - x - y) - km1_val * x - kp2_val * ((1 - x - y) ** 2) * x
+    dy_dt = kp3_val * (1 - x - y) ** 2 - km3_val * (y ** 2)
+    return [dx_dt, dy_dt]
+
+def make_system_2(initial_vals, time):
+    x = initial_vals[0]
+    y = initial_vals[1]
+    dx_dt = kp1_function[point2] * (1 - x - y) - km1_val * x - kp2_val * ((1 - x - y) ** 2) * x
+    dy_dt = kp3_val * (1 - x - y) ** 2 - km3_val * (y ** 2)
+    return [dx_dt, dy_dt]
+
+initial_values = [x_first, y_first]
+t = np.linspace(0, 5000, 50000)
+result = odeint(make_system_1, initial_values, t)
+
+plt.plot(t, result[:, 0], label='x')
+plt.plot(t, result[:, 1], label='y')
+plt.title("Численное решение при k_1 =" + str(kp1_function[point1]))
+plt.xlabel("t")
+plt.show()
+
+plt.plot(result[:, 0], result[:, 1])
+plt.title("Результат №1 в фазовой плоскости")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+initial_values = [x_second, y_second]
+t = np.linspace(0, 5000, 50000)
+result = odeint(make_system_2, initial_values, t)
+
+plt.plot(t, result[:, 0], label='x')
+plt.plot(t, result[:, 1], label='y')
+plt.title("Численное решение при k_1 =" + str(kp1_function[point2]))
+plt.xlabel("t")
+plt.legend()
+plt.show()
+
+plt.plot(result[:, 0], result[:, 1])
+plt.title("Результат №2 в фазовой плоскости")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.show()
+
+res = solve([trace_jacobian_wo_x], kp1)
 kp1_again_from_trace = res[kp1]
 
-res = solve([det_jacA_wo_x], kp1)
+res = solve([det_jacobian_wo_x], kp1)
 kp1_again_from_det = res[kp1]
 
 res1 = solve([kp1_again_from_trace - kp1_eq], km1)
-km1_again_from_trace_tilda = res1[km1]
+km1_from_trace = res1[km1]
 
 res2 = solve([kp1_again_from_det - kp1_eq], km1)
-km1_again_from_det_tilda_tilda = res2[km1]
+km1_from_det = res2[km1]
 
-km1_from_trace_func = lambdify((y, kp1, kp2, km3, kp3), km1_again_from_trace_tilda)
-km1_from_dim_func = lambdify((y, kp1, kp2, km3, kp3), km1_again_from_det_tilda_tilda)
+km1_from_trace_lambdify = lambdify((y, kp1, kp2, km3, kp3), km1_from_trace)
+km1_from_det_lambdify = lambdify((y, kp1, kp2, km3, kp3), km1_from_det)
 
-km1_from_trace_func_f = km1_from_trace_func(np.linspace(0.01, 0.09, 1001), kp1_val, kp2_val, km3_val, kp3_val)
-km1_from_dim_func_f = km1_from_dim_func(np.linspace(0.01, 0.09, 1001), kp1_val, kp2_val, km3_val, kp3_val)
+km1_from_trace_function = km1_from_trace_lambdify(np.linspace(0.01, 0.9, 1000), kp1_val, kp2_val, km3_val, kp3_val)
+km1_from_det_function = km1_from_det_lambdify(np.linspace(0.01, 0.9, 1000), kp1_val, kp2_val, km3_val, kp3_val)
 
-plt.plot(np.linspace(0.01, 1, 1001), km1_from_trace_func_f)
-plt.xlabel("y")
-plt.ylabel("km1_from_trace_func_f")
+plt.plot(kp1_function, km1_from_trace_function, label='k_(-1) на линии кратности')
+plt.plot(kp1_function, km1_from_det_function, label='k_(-1) на линии нейтральности')
+plt.title("Параметрический портрет исходной системы")
+plt.xlabel("k_1")
+plt.xlim([0.118, 0.130])
+plt.ylim([0.0, 0.02])
+plt.legend()
 plt.show()
 
-plt.plot(np.linspace(0.01, 1, 1001), km1_from_dim_func_f)
-plt.xlabel("y")
-plt.ylabel("km1_from_dim_func_f")
+kp1_point = 0.125
+km1_point = 0.005
+
+def make_system_3(initial_vals, time):
+    x = initial_vals[0]
+    y = initial_vals[1]
+    dx_dt = kp1_point * (1 - x - y) - km1_point * x - kp2_val * ((1 - x - y) ** 2) * x
+    dy_dt = kp3_val * (1 - x - y) ** 2 - km3_val * (y ** 2)
+    return [dx_dt, dy_dt]
+
+initial_values = [x_first, y_first]
+t = np.linspace(0, 1000, 10000)
+result = odeint(make_system_3, initial_values, t)
+
+plt.plot(t, result[:, 0], label='x')
+plt.plot(t, result[:, 1], label='y')
+plt.title("Численное решение при k_1 = " + str(kp1_point) + " и k_(-1) =" + str(km1_point))
+plt.xlabel("t")
+plt.legend()
 plt.show()
 
-plt.plot(func, km1_from_trace_func_f)
-plt.xlabel("kp1")
-plt.ylabel("km1_from_trace")
-plt.show()
-
-plt.plot(func, km1_from_dim_func_f)
-plt.xlabel("kp1")
-plt.ylabel("km1_from_dim")
+plt.plot(result[:, 0], result[:, 1])
+plt.title("Фазовый портрет системы в области автоколебаний")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
